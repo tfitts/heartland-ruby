6a05b26318dd90af4b1fe22488fa817aa1adb810
tfitts <tfitts@gmail.com>
2017-06-01 10:21:17 -0600

    Allow initialize HpsCardholder object with hash

--- lib/hps/entities/hps_cardholder.rb
+++ lib/hps/entities/hps_cardholder.rb
@@ -2,6 +2,15 @@ module Hps
 	class HpsCardHolder
 
 		attr_accessor :first_name, :last_name, :phone, :email_address, :address
+		
+		def initialize params = {}
+			@first_name = params[:first_name]
+			@last_name = params[:last_name]
+			@phone = params[:phone]
+			@email_address = params[:email_address]
+			@email_address ||= params[:email]
+			@address = params[:address]
+		end
 
 	end
 end
